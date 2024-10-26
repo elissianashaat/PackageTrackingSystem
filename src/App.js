c13db3dc01a81bcb0b0112690f20a64a32089c30
elissianashaat <91636261+elissianashaat@users.noreply.github.com>
2024-10-26 12:46:42 +0300

    Add files via upload
    login sign up frontend

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import LoginSignUp from './components/loginsignup/LoginSignUp';
+
+
+function App() {
+  return (
+    <div>
+      <LoginSignUp />
+    </div>
+  );
+}
+
+export default App;
